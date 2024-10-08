5f884a046a299df7b41f53208cf8e21bcb776d8b
flesten-ali <fbada858@gmail.com>
2024-10-08 23:57:40 +0300

    Add Products To MongoDB

---
+++ FalastinShop.SIMS/MongoDB/Models/CurrencyDoc.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FalastinShop.SIMS.MongoDB.Models;
+
+public class CurrencyDoc
+{
+    [BsonId]
+    public ObjectId CurrencyId {  get; set; }
+  
+    public string CurrencyCode { get; set; }
+}
