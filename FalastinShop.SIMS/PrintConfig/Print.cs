23f96c5942a9f82e2770fcbcf5710bb9530578d0
flesten-ali <fbada858@gmail.com>
2024-10-08 18:47:45 +0300

    Apply changes to the code to use DataBase

--- FalastinShop.SIMS/PrintConfig/Print.cs
+++ FalastinShop.SIMS/PrintConfig/Print.cs
@@ -1,19 +1,21 @@
 ﻿namespace FalastinShop.SIMS.PrintConfig;
 public static class Print
 {
-   public static void ConfigSuccessConsole(string msg)
+    public static void ConfigSuccessConsole(string msg)
     {
         ConfigConsoleColor(msg, ConsoleColor.Green);
     }
+
     public static void ConfigErrorConsole(string msg)
     {
         ConfigConsoleColor(msg, ConsoleColor.Red);
-
     }
+
     private static void Log(string msg)
     {
         Console.WriteLine(msg);
     }
+
     private static void ConfigConsoleColor(string msg, ConsoleColor color)
     {
         Console.ForegroundColor = color;
@@ -21,4 +23,3 @@ public static class Print
         Console.ResetColor();
     }
 }
- 
--- FalastinShop.SIMS/ProductManagment/Product.cs
+++ FalastinShop.SIMS/ProductManagment/Product.cs
@@ -1,19 +1,17 @@
 ﻿namespace FalastinShop.SIMS.ProductManagment;
 public class Product
-     {
-        public Guid Id { get; }
-        public string Name { get;   set; } 
-        public int Quantity { get;   set; }  
-        public Price Price { get;   set; }
- 
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public Price Price { get; set; }
 
-         public Product(string name, int quantity, double itemPrice ,  Currency currency)
-         {
-            Id = Guid.NewGuid();
-            Name = name;
-            Quantity = quantity;
-            Price = new Price() { ItemPrice  = itemPrice, Currency = currency};
-           
-         }
+    public Product() { }
+
+    public Product(string name, int quantity, double itemPrice, Currency currency)
+    {
+        Name = name;
+        Quantity = quantity;
+        Price = new Price() { ItemPrice = itemPrice, Currency = currency };
     }
- 
+}
