d327733b2066e2a4d58afa55191a8827adb93295
flesten-ali <fbada858@gmail.com>
2024-10-09 00:41:17 +0300

    Add Products To MongoDB

---
+++ FalastinShop.SIMS/MongoDB/Models/ProductDoc.cs
@@ -0,0 +1,17 @@
+﻿using FalastinShop.SIMS.ProductManagment;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FalastinShop.SIMS.MongoDB.Models;
+
+public class ProductDoc
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    public string Name {  get; set; }
+    public int Quantity { get; set; }   
+    public double Price {  get; set; }
+
+    public ObjectId CurrencyId {  get; set; }
+}
